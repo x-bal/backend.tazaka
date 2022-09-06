69947b0299b5d9cbe20e368229af225003353043
mxbal <mxbal026@gmail.com>
2022-09-06 08:57:13 +0700

    Update Landing Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,11 +64,12 @@
         <div class="container-fluid" data-aos="fade-up">
             <div class="row justify-content-center">
                 <div class="col-xl-5 col-lg-6 pt-3 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                    <h1 id="home-title"></h1>
-                    <h2 id="home-sub_title"></h2>
+                    <h1>{{ App\Models\Home::find(1)->title ?? '-' }}</h1>
+                    <h2>{{ App\Models\Home::find(1)->sub_title ?? '-' }}</h2>
                     <div><a href="#about" class="btn-get-started scrollto">Get Started</a></div>
                 </div>
-                <div class="col-xl-4 col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="150" id="home-hero">
+                <div class="col-xl-4 col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="150">
+                    <img src="{{ asset('/storage') }}/{{ App\Models\Home::find(1)->image ?? '-' }}" class="img-fluid animated" alt="">
                 </div>
             </div>
         </div>
@@ -83,15 +84,17 @@
             <div class="container">
 
                 <div class="row">
-                    <div class="col-lg-6 text-center order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="150" id="about-hero">
-
+                    <div class="col-lg-6 text-center order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="150">
+                        <img src="{{ asset('/storage') }}/{{ App\Models\About::find(1)->image ?? '-' }}" class="" width="250" alt="">
                     </div>
                     <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
-                        <h3 id="about-title"></h3>
-                        <p class="fst-italic" id="about-sub_title">
-
+                        <h3>{{ App\Models\About::find(1)->title ?? '-' }}</h3>
+                        <p class="fst-italic">
+                            {{ App\Models\About::find(1)->sub_title ?? '-' }}
                         </p>
-                        <p id="about-content"></p>
+                        <p>
+                            {{ App\Models\About::find(1)->content ?? '-' }}
+                        </p>
                         <a href="#" class="read-more">Read More <i class="bi bi-long-arrow-right"></i></a>
                     </div>
                 </div>
@@ -103,7 +106,7 @@
         <section id="counts" class="counts">
             <div class="container">
 
-                <div class="row counters" id="list-count">
+                <div class="row counters">
 
                     @foreach(App\Models\Counts::get() as $count)
                     <div class="col-lg-3 col-6 text-center">
@@ -126,8 +129,22 @@
                     <p>{{ App\Models\Section::where('section', 'Service')->first()->content ?? 'This is our service' }}</p>
                 </div>
 
-                <div class="row gy-4" id="service-list">
-
+                <div class="row gy-4">
+                    @foreach(App\Models\Service::get() as $service)
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="icon-box iconbox-blue" style="min-width: 100%;">
+                            <div class="icon">
+                                <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,582.0697525312426C382.5290701553225,586.8405444964366,449.9789794690241,525.3245884688669,502.5850820975895,461.55621195738473C556.606425686781,396.0723002908107,615.8543463187945,314.28637112970534,586.6730223649479,234.56875336149918C558.9533121215079,158.8439757836574,454.9685369536778,164.00468322053177,381.49747125262974,130.76875717737553C312.15926192815925,99.40240125094834,248.97055460311594,18.661163978235184,179.8680185752513,50.54337015887873C110.5421016452524,82.52863877960104,119.82277516462835,180.83849132639028,109.12597500060166,256.43424936330496C100.08760227029461,320.3096726198365,92.17705696193138,384.0621239912766,124.79988738764834,439.7174275375508C164.83382741302287,508.01625554203684,220.96474134820875,577.5009287672846,300,582.0697525312426">
+                                    </path>
+                                </svg>
+                                {!! $service->icon !!}
+                            </div>
+                            <h4><a href="">{{ $service->title }}</a></h4>
+                            <p>{{ $service->description }}</p>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
 
             </div>
@@ -142,8 +159,16 @@
                     <p>{{ App\Models\Section::where('section', 'Client')->first()->content ?? 'This is our clients' }}</p>
                 </div>
 
-                <div class="row" id="list-client">
+                <div class="row">
+                    @foreach(App\Models\Client::get() as $client)
+                    <div class="col-md-3 mb-3">
+                        <img src="{{ asset('/storage/'.$client->logo) }}" alt="" class="img-fluid">
 
+                        <div class="mt-3 text-center">
+                            <h5>{{ $client->name }}</h5>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
 
             </div>
@@ -159,7 +184,20 @@
                 </div>
 
                 <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-                    <div class="swiper-wrapper" id="list-testimonial">
+                    <div class="swiper-wrapper">
+                        @foreach(App\Models\Testimonial::get() as $testi)
+                        <div class="swiper-slide">
+                            <div class="testimonial-item">
+                                <p>
+                                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                    {{ $testi->desc }}
+                                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                </p>
+                                <img src="{{ asset('/storage/'.$testi->image) }}" class="testimonial-img" alt="">
+                                <h3>{{ $testi->name }}</h3>
+                            </div>
+                        </div>
+                        @endforeach
 
                     </div>
                     <div class="swiper-pagination"></div>
@@ -198,7 +236,7 @@
                                 <p>{{ $portfolio->category->name }}</p>
                             </div>
                             <div class="portfolio-links">
-                                <a href="{{ asset('storage/'. $portfolio->image) }}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="{{ $portfolio->name }}"><i class="bx bx-plus"></i></a>
+                                <a href="{{ asset('storage/'. $portfolio->image) }}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="{{ $portfolio->name }}"></a>
                                 <a href="#" title="More Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
@@ -217,26 +255,57 @@
                     <p>{{ App\Models\Section::where('section', 'Team')->first()->content ?? 'This is our team' }}</p>
                 </div>
 
-                <div class="row" id="list-team">
-
+                <div class="row">
+                    @foreach(App\Models\Team::get() as $team)
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="100">
+                            <div class="member-img">
+                                <img src="{{ asset('/storage/'.$team->image) }}" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href="{{ $team->twitter }}"><i class="bi bi-twitter"></i></a>
+                                    <a href="{{ $team->facebook }}"><i class="bi bi-facebook"></i></a>
+                                    <a href="{{ $team->instagram }}"><i class="bi bi-instagram"></i></a>
+                                    <a href="{{ $team->linkedin }}"><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>{{ $team->name }}</h4>
+                                <span>{{ $team->position }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
 
             </div>
         </section>
 
         <!-- ======= Procuct Section ======= -->
-        <section id="product" class="procuct section-bg">
-            <div class="container" data-aos="fade-up">
+        <section id="product" class="team section-bg">
+            <div class="container">
 
-                <div class="section-title">
+                <div class="section-title" data-aos="fade-up">
                     <h2>Our Product</h2>
                     <p>{{ App\Models\Section::where('section', 'Product')->first()->content ?? 'This is our product' }}</p>
                 </div>
 
-                <div class="row" id="list-product">
-
+                <div class="row">
+                    @foreach(App\Models\Product::get() as $product)
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="100">
+                            <div class="member-img">
+                                <img src="{{ asset('/storage/'.$product->image) }}" class="img-fluid" alt="">
+                                <div class="social">
+                                    <p>{{ $product->description }}</p>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>{{ $product->name }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
-
             </div>
         </section><!-- End Procuct Section -->
 
